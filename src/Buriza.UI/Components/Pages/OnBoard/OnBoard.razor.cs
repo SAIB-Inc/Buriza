5674d40efa3e9dbf359b8fa81aad5d43f79a8d7e
KeeeeEf <kfrgallego@gmail.com>
2025-08-26 18:04:33 +0800

    feat(ui): add navigation functionality to onboarding flow

--- src/Buriza.UI/Components/Pages/OnBoard/OnBoard.razor.cs
+++ src/Buriza.UI/Components/Pages/OnBoard/OnBoard.razor.cs
@@ -1,4 +1,5 @@
 using MudBlazor;
+using Microsoft.AspNetCore.Components;
 
 namespace Buriza.UI.Components.Pages;
 
@@ -8,9 +9,76 @@ public partial class OnBoard
     protected MudCarousel<object>? _carousel;
     protected bool IsLastSlide => _carousel != null && _carousel.SelectedIndex == _carousel.Items.Count - 1;
     protected List<string> RecoveryPhrase = ["random", "quack", "taught", "light", "short", "hard", "help", "please", "earth", "cake", "control", "guard", "trust", "frost", "echo", "swim", "give", "seen", "eyes", "wade", "ice", "explain", "water", "geese"];
+    
+    private int _currentSlide = 0;
+    protected int CurrentSlide 
+    { 
+        get => _currentSlide;
+        set
+        {
+            if (_currentSlide != value)
+            {
+                _currentSlide = value;
+                StateHasChanged();
+            }
+        }
+    }
+
+    protected string SecondaryButtonText => CurrentSlide switch
+    {
+        0 => "Import Using Seed Phrase",
+        1 => "Copy to Clipboard",
+        2 => "Paste from Clipboard",
+        _ => string.Empty
+    };
+
+    protected string PrimaryButtonText => CurrentSlide switch
+    {
+        0 => "Create New Wallet",
+        1 => "Next",
+        2 => "Next",
+        3 => "Save Wallet",
+        4 => "Continue",
+        5 => "Continue",
+        _ => "Next"
+    };
+
+    protected bool ShowSecondaryButton => CurrentSlide <= 2;
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        if (firstRender && _carousel != null)
+        {
+            _carousel.SelectedIndexChanged = EventCallback.Factory.Create<int>(this, OnSlideChanged);
+        }
+        base.OnAfterRender(firstRender);
+    }
+
+    private void OnSlideChanged(int newIndex)
+    {
+        CurrentSlide = newIndex;
+    }
 
     private void OnNextButtonClicked()
     {
         _carousel?.Next();
+        if (_carousel != null)
+        {
+            CurrentSlide = _carousel.SelectedIndex;
+        }
+    }
+
+    private void OnSecondaryButtonClicked()
+    {
+        // TODO: Implement secondary button actions
+    }
+
+    private void OnBackButtonClicked()
+    {
+        _carousel?.Previous();
+        if (_carousel != null)
+        {
+            CurrentSlide = _carousel.SelectedIndex;
+        }
     }
 }
