97ecffe8e782790c9781361783c09d48bf61a8f0
KeeeeEf <kfrgallego@gmail.com>
2025-08-28 01:10:48 +0800

    feat(ui): added authorization ui and new background image

--- src/Buriza.UI/Components/Pages/Dapp/Dapp.razor.cs
+++ src/Buriza.UI/Components/Pages/Dapp/Dapp.razor.cs
@@ -2,5 +2,17 @@ namespace Buriza.UI.Components.Pages;
 
 public partial class Dapp
 {
+    protected int ExpandedCard = 1;
+    protected bool ShowAuthorization = false;
+    protected string SelectedCategory = "All";
     
+    protected void ExpandCard(int cardIndex)
+    {
+        ExpandedCard = cardIndex;
+    }
+    
+    protected void OnDappCardClicked()
+    {
+        ShowAuthorization = true;
+    }
 }
