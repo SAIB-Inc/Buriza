d43d0c56e2ab01f42011e35108df3fdf7a109725
Kiefer Gallego <110240392+KeeeeEf@users.noreply.github.com>
2025-08-29 01:06:18 +0800

    feat(ui): home screen implementation (#8)
    * feat(ui): splash screen implementation
    
    * feat(ui): update background images and implement dark mode support
    
    * feat(ui): home screen implementation
    
    * feat(ui): header implementation and ui adjustments on home page
    
    * fix(web): adjust spacing and width for buriza asset card component
    
    * feat(ui): add BurizaHeader component and update MainLayout; adjust styles and remove unused carousel logic
    
    * fix(ui): adjust padding and spacing in Home component for improved layout
    
    * fix(ui): update styles and layout for BurizaTabs and BurizaAssetCard components; adjust Home page UI elements
    
    * fix(assets): update ada.webp image asset
    
    * feat(ui): ui adjustments for extension

--- src/Buriza.UI/Components/Layout/MainLayout.razor.cs
+++ src/Buriza.UI/Components/Layout/MainLayout.razor.cs
@@ -32,6 +32,7 @@ public partial class MainLayout
             Secondary = "#002456",
             SecondaryDarken = "#001A42",
             SecondaryContrastText = "#AEC6FF",
+            Tertiary = "#00173D",
             TextPrimary = "#E0E2ED",
             TextSecondary = "#8490B1",
             GrayLighter = "#AFAFAF",
@@ -42,7 +43,9 @@ public partial class MainLayout
             DarkLighten = "#8B90A0",
             Dark = "#181B23",
             TableLines = "#23304B",
-            Success = "#00B286"
+            SuccessLighten = "#71FAC9",
+            Success = "#00B286",
+            SuccessDarken = "#002116"
         },
         PaletteLight = new()
         {
@@ -55,6 +58,7 @@ public partial class MainLayout
             SecondaryLighten = "#A6C1FE",
             Secondary = "#0057C0",
             SecondaryContrastText = "#324E83",
+            Tertiary = "#FEFCFF",
             TextPrimary = "#181B23",
             TextSecondary = "#515E7C",
             GrayLighter = "#AFAFAF",
@@ -65,7 +69,9 @@ public partial class MainLayout
             DarkLighten = "#C1C6D7",
             Dark = "#ECEDF8",
             TableLines = "#B9C6E9",
-            Success = "#00A663"
+            SuccessLighten = "#77FBAF",
+            Success = "#00A663",
+            SuccessDarken = "#005AC4"
         }
     };
 
