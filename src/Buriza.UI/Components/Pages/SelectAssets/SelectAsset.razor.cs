ec606ef1145e6482b0be0f7b8fedc836acecb80c
KeeeeEf <kfrgallego@gmail.com>
2025-09-03 22:45:22 +0800

    feat(data): add TokenEntry and Recipient models for asset transactions

--- src/Buriza.UI/Components/Pages/SelectAssets/SelectAsset.razor.cs
+++ src/Buriza.UI/Components/Pages/SelectAssets/SelectAsset.razor.cs
@@ -1,6 +1,10 @@
+using Buriza.UI.Services;
+using Microsoft.AspNetCore.Components;
+
 namespace Buriza.UI.Components.Pages;
 
 public partial class SelectAsset
 {
-
+    [Inject]
+    public required AppStateService AppStateService { get; set; }
 }
