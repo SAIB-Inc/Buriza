759f896c91407bcd074a66f02479f69e188433d7
CML90 <caitlincebu@hotmail.com>
2025-09-04 22:23:16 +0800

    feat(ui): implement carousel for receive page layout

--- src/Buriza.UI/Components/Pages/Receive/Receive.razor.cs
+++ src/Buriza.UI/Components/Pages/Receive/Receive.razor.cs
@@ -8,4 +8,8 @@ public partial class Receive
 {
     [Inject]
     public required AppStateService AppStateService { get; set; }
+
+    protected MudCarousel<object>? _carousel;
+
+    protected int CurrentSlide { get; set; } = 0;
 }
