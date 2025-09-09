82231c182746ee5d9ba6a303089255bac8fd3d62
KeeeeEf <kfrgallego@gmail.com>
2025-09-10 01:34:40 +0800

    feat(ui): implement filter drawer toggle and screen width detection for history page

--- src/Buriza.UI/Components/Pages/History/History.razor.cs
+++ src/Buriza.UI/Components/Pages/History/History.razor.cs
@@ -1,6 +1,38 @@
+using Buriza.UI.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
 namespace Buriza.UI.Components.Pages;
 
 public partial class History
 {
+    [Inject]
+    public required AppStateService AppStateService { get; set; }
+    
+    [Inject]
+    public required IJSRuntime JSRuntime { get; set; }
+    
     protected bool IsSummaryDisplayed { get; set; }
+    
+    protected void ToggleFilterDrawer()
+    {
+        AppStateService.IsFilterDrawerOpen = !AppStateService.IsFilterDrawerOpen;
+    }
+
+    protected async Task HandleTransactionClick()
+    {
+        // Check screen width
+        var screenWidth = await JSRuntime.InvokeAsync<int>("getScreenWidth");
+        
+        if (screenWidth >= 1024) // lg breakpoint
+        {
+            // Desktop: Only open drawer
+            AppStateService.IsFilterDrawerOpen = true;
+        }
+        else
+        {
+            // Mobile: Show summary content
+            IsSummaryDisplayed = true;
+        }
+    }
 }
