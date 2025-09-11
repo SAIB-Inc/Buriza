a2f36569fef9505c26472a474d362c75f1debfa7
KeeeeEf <kfrgallego@gmail.com>
2025-09-12 01:59:12 +0800

    feat(ui): dapp screen desktop implementation

--- src/Buriza.UI/Components/Pages/Dapp/Dapp.razor.cs
+++ src/Buriza.UI/Components/Pages/Dapp/Dapp.razor.cs
@@ -3,12 +3,18 @@ namespace Buriza.UI.Components.Pages;
 public partial class Dapp
 {
     protected int ExpandedCard = 1;
+    protected int ExpandedDesktopSlide = 0;
     protected bool ShowAuthorization = false;
     protected string SelectedCategory = "All";
     
     protected void ExpandCard(int cardIndex)
     {
         ExpandedCard = cardIndex;
+    }
+    
+    protected void ExpandDesktopSlide(int slideIndex)
+    {
+        ExpandedDesktopSlide = slideIndex;
     }
     
     protected void OnDappCardClicked()
