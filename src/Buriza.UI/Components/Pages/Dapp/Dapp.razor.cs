2bb0eaab5a4f2e026d78887e64370f7f1ec0ab1e
Kiefer Gallego <110240392+KeeeeEf@users.noreply.github.com>
2025-09-12 02:29:48 +0800

    feat(ui): dapp screen desktop implementation (#24)
    * feat(ui): dapp screen desktop implementation
    
    * feat(ui): implement dapp authorization ui and carousel functionality

--- src/Buriza.UI/Components/Pages/Dapp/Dapp.razor.cs
+++ src/Buriza.UI/Components/Pages/Dapp/Dapp.razor.cs
@@ -1,8 +1,19 @@
+using Buriza.UI.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
 namespace Buriza.UI.Components.Pages;
 
 public partial class Dapp
 {
+    [Inject]
+    public required AppStateService AppStateService { get; set; }
+    
+    [Inject]
+    public required IJSRuntime JSRuntime { get; set; }
+    
     protected int ExpandedCard = 1;
+    protected int ExpandedDesktopSlide = 0;
     protected bool ShowAuthorization = false;
     protected string SelectedCategory = "All";
     
@@ -11,8 +22,22 @@ public partial class Dapp
         ExpandedCard = cardIndex;
     }
     
-    protected void OnDappCardClicked()
+    protected void ExpandDesktopSlide(int slideIndex)
     {
-        ShowAuthorization = true;
+        ExpandedDesktopSlide = slideIndex;
+    }
+    
+    protected async Task OnDappCardClicked()
+    {
+        int screenWidth = await JSRuntime.InvokeAsync<int>("getScreenWidth");
+        
+        if (screenWidth >= 1024)
+        {
+            AppStateService.IsFilterDrawerOpen = true;
+        }
+        else
+        {
+            ShowAuthorization = true;
+        }
     }
 }
