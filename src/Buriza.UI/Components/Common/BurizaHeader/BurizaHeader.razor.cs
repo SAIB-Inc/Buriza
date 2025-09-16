bb1dbcadee5601bcb76d90af10a6161f00b8e80d
KeeeeEf <kfrgallego@gmail.com>
2025-09-16 20:51:22 +0800

    fix(ui): adjust header pop up positioning and clean ups

--- src/Buriza.UI/Components/Common/BurizaHeader/BurizaHeader.razor.cs
+++ src/Buriza.UI/Components/Common/BurizaHeader/BurizaHeader.razor.cs
@@ -2,43 +2,28 @@ using Microsoft.AspNetCore.Components;
 
 namespace Buriza.UI.Components.Common;
 
-public partial class BurizaHeader : IDisposable
+public partial class BurizaHeader
 {
     [Inject]
     public required NavigationManager Navigation { get; set; }
     
-    private bool isSettingsVisible = false;
+    protected bool IsPopupVisible = false;
     
-    protected override void OnInitialized()
+    protected void TogglePopup()
     {
-        Navigation.LocationChanged += HandleLocationChanged;
+        IsPopupVisible = !IsPopupVisible;
+    }
+
+    protected void ClosePopup()
+    {
+        IsPopupVisible = false;
     }
     
-    private void HandleLocationChanged(object? sender, Microsoft.AspNetCore.Components.Routing.LocationChangedEventArgs e)
-    {
-        StateHasChanged();
-    }
-    
-    private void ToggleSettings()
-    {
-        isSettingsVisible = !isSettingsVisible;
-    }
-    
-    private void CloseSettings()
-    {
-        isSettingsVisible = false;
-    }
-    
-    private string GetSpanClass(string route)
+    protected string GetSpanClass(string route)
     {
         bool isActive = Navigation.Uri.Contains(route) || (route == "/" && (Navigation.Uri.EndsWith("/") || Navigation.Uri.Contains("/popup") || Navigation.Uri.Contains("/options") || Navigation.Uri.Contains("/index")));
         return isActive 
             ? "text-[11px] !text-[var(--mud-palette-primary-text)] !bg-[var(--mud-palette-primary-lighten)] !rounded-full !px-1 !py-[2px]"
             : "text-[11px] !text-[var(--mud-palette-text-secondary)] !px-1 !py-[2px]";
-    }
-    
-    public void Dispose()
-    {
-        Navigation.LocationChanged -= HandleLocationChanged;
     }
 }
