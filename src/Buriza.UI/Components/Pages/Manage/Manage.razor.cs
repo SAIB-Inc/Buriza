28aac052bf2c86e6bd2c9852ed618504b2860565
Kiefer Gallego <110240392+KeeeeEf@users.noreply.github.com>
2026-01-16 02:06:34 +0800

    fix(ui): remove redundant initialization and StateHasChanged in Manage page (#75)

--- src/Buriza.UI/Components/Pages/Manage/Manage.razor.cs
+++ src/Buriza.UI/Components/Pages/Manage/Manage.razor.cs
@@ -7,36 +7,35 @@ public partial class Manage
 {
     protected bool IsAccountFormVisible { get; set; }
     protected bool IsEditMode { get; set; }
-    
-    protected List<Wallet> wallets = WalletData.GetDummyWallets();
-    protected bool[] WalletExpandedStates = new bool[3];
-    
+
+    protected List<Wallet> Wallets { get; set; } = [];
+    protected bool[] WalletExpandedStates { get; set; } = new bool[3];
+
     protected override void OnInitialized()
     {
-        wallets = WalletData.GetDummyWallets();
+        Wallets = WalletData.GetDummyWallets();
     }
-    
+
     protected void ShowAddAccount()
     {
         IsEditMode = false;
         IsAccountFormVisible = true;
     }
-    
+
     protected void ShowEditWallet()
     {
         IsEditMode = true;
         IsAccountFormVisible = true;
     }
-    
+
     protected void HideAccountForm()
     {
         IsAccountFormVisible = false;
         IsEditMode = false;
     }
-    
+
     protected void ToggleWallet(int walletIndex)
     {
         WalletExpandedStates[walletIndex] = !WalletExpandedStates[walletIndex];
-        StateHasChanged();
     }
 }
